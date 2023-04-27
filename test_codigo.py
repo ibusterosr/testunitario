2b94a1fe344dc50bae2bff9f8738fb6d993ff9d4
Ibusteros <ibusterosr193@gmail.com>
2023-04-27 12:03:49 +0200

    cambio2

--- test_codigo.py
+++ test_codigo.py
@@ -3,3 +3,7 @@ import codigo
 def test_suma():
     assert codigo.suma(1,1)==2
     assert codigo.suma(0,-1)==-1
+
+def test_resta():
+    assert codigo.resta(1,1)==0
+    assert codigo.resta(0,-1)==1
